a27dfcb043ca0fe6429f8c699db1865e7bc89f59
magcicada <l1334938030@gmail.com>
2023-12-19 20:38:35 +0800

    Update DualityInterface.java

--- src/main/java/appeng/helpers/DualityInterface.java
+++ src/main/java/appeng/helpers/DualityInterface.java
@@ -1219,6 +1219,13 @@ public class DualityInterface implements IGridTickable, IStorageMonitorable, IIn
 
         for (final ItemStack is : this.storage) {
             if (!is.isEmpty()) {
+                int maxStackSize = is.getMaxStackSize();
+                while (is.getCount() > maxStackSize) {
+                    ItemStack portionedStack = is.copy();
+                    portionedStack.setCount(maxStackSize);
+                    is.shrink(maxStackSize);
+                    drops.add(portionedStack);
+                }
                 drops.add(is);
             }
         }
